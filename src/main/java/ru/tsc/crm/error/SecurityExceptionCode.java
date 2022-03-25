dad34841cf846dd6a715860b2fa9c445352fe137
Вдовин Юрий Игоревич <yvdovin@t1-consulting.ru>
2022-03-25 15:54:32 +0300

    поднял версию утил либы 3.12.0

--- src/main/java/ru/tsc/crm/error/SecurityExceptionCode.java
+++ src/main/java/ru/tsc/crm/error/SecurityExceptionCode.java
@@ -5,13 +5,13 @@ import lombok.Getter;
 import ru.tsc.crm.error.exception.SecurityException;
 import ru.tsc.crm.error.exception.code.ExceptionCode;
 
-import static ru.tsc.crm.quarkus.error.message.SecurityExceptionMessage.INVALID_COOKIE;
+
 
 @AllArgsConstructor
 @Getter
 public enum SecurityExceptionCode implements ExceptionCode<SecurityException> {
 
-    SESSION_ID_IS_ABSENT("01", INVALID_COOKIE),
+    SESSION_ID_IS_ABSENT("01", "Кука  sessionId отсутствует"),
     SESSION_DATA_NOT_FOUND("02", "Данные сессии не найдены"),
     METHOD_ACCESS_DENIED("02", "Вызываемый метод не связан с Ролью Пользователя"),
     SUB_METHOD_ACCESS_DENIED("03", "Вызываемый подметод не связан с Ролью Пользователя"),
